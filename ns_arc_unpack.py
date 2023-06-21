9eeef9a5d3eec5686f5cee4add25b25c0ef64a64
萌泪酱 <605738729@qq.com>
2023-06-22 01:44:17 +0800

    Init commit.

--- ns_arc_unpack.py
+++ ns_arc_unpack.py
@@ -3,31 +3,36 @@ import sys
 import json
 import os
 
+originPath = r'F:\NS-Unpack\NCA-NSP-XCI_TO_LayeredFS_v1.6\Updated_LayeredFS\romfs'
 dumpPath = "dump"
 
-if not os.path.exists(dumpPath):
-    os.mkdir(dumpPath)
-    
-arcpack = open(sys.argv[2],'rb')
 
-with open(sys.argv[1], 'r') as arcjson:
-    data = json.load(arcjson)
-    for group in data["Groups"]:
-        groupPath = dumpPath + "/" + group["Name"]
-        if not os.path.exists(groupPath):
-            os.mkdir(groupPath)
-
-        for OrderedEntrie in group["OrderedEntries"]:
-            filePath = groupPath + "/" + OrderedEntrie["OriginalFilename"]
-            if not os.path.exists(filePath[0:filePath.rfind("/")]):
-                os.makedirs(filePath[0:filePath.rfind("/")])
-
-            arcpack.seek(OrderedEntrie["Offset"])
-            with open(filePath, 'wb') as dumpFile:
-                dumpFile.write(arcpack.read(OrderedEntrie["Length"]))
-                dumpFile.close()
+def unpack(input_json, input_pack):
+    with open(input_pack, 'rb') as arc_pack:
+        with open(input_json, 'r') as arc_json:
+            data = json.load(arc_json)
+            for group in data["Groups"]:
+                group_path = dumpPath + "/" + group["Name"]
+                if not os.path.exists(group_path):
+                    os.mkdir(group_path)
+                for OrderedEntry in group["OrderedEntries"]:
+                    file_path = group_path + "/" + OrderedEntry["OriginalFilename"]
+                    if not os.path.exists(file_path[0:file_path.rfind("/")]):
+                        os.makedirs(file_path[0:file_path.rfind("/")])
+                    arc_pack.seek(OrderedEntry["Offset"])
+                    with open(file_path, 'wb') as dumpFile:
+                        dumpFile.write(arc_pack.read(OrderedEntry["Length"]))
+                        dumpFile.close()
+                    print("提取完毕：" + file_path)
 
 
-    arcjson.close()
-
-arcpack.close()
+if __name__ == '__main__':
+    if not os.path.exists(dumpPath):
+        os.mkdir(dumpPath)
+    for root, dirs, files in os.walk(originPath):
+        for file in files:
+            path = os.path.join(root, file)
+            if path.endswith('.json'):
+                json_file = path
+                pack_file = path.replace('.json', '.pack')
+                unpack(json_file, pack_file)
